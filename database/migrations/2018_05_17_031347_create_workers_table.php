8dce50e3265f7f9ce335dfe9a92e3652b1bec041
Rostik G <g13338@gmail.com>
2018-05-17 06:40:18 +0300

    Model structure

--- database/migrations/2018_05_17_031347_create_workers_table.php
+++ database/migrations/2018_05_17_031347_create_workers_table.php
@@ -15,6 +15,12 @@ class CreateWorkersTable extends Migration
     {
         Schema::create('workers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('secondname');
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->year('birth_year');
+            $table->string('position');
+            $table->integer('salary');
             $table->timestamps();
         });
     }
